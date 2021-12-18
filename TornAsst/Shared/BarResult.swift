7f596b986fd3a74f288f5f169d7895d746f8b8c1
Bryan Costanza <bryancostanza@icloud.com>
2021-12-17 17:08:54 -0700

    Working checkpoint

---
+++ TornAsst/Shared/BarResult.swift
@@ -0,0 +1,19 @@
+//
+//  BarResult.swift
+//  TornAsst
+//
+//  Created by Bryan Costanza on 1/31/21.
+//
+
+import Foundation
+
+struct BarResult: Codable, TickDefinite {
+    var current: Int
+    var maximum: Int
+    var increment: Int
+    var interval: Seconds
+    var ticktime: Seconds
+    var fulltime: Seconds
+    
+    static var `default` = BarResult(current: 75, maximum: 100, increment: 5, interval: 900, ticktime: 452, fulltime: 4952)
+}
